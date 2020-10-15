ed32a41133c8294c88eb62cc9e8d72f90e693b11
Alex <gubanov1514@gmail.com>
2020-10-15 14:48:00 +0300

    Cleaned up tests

--- Sources/OverAssistant/OverAssistant.swift
+++
@@ -1,3 +0,0 @@
-struct OverAssistant {
-    var text = "Hello, World!"
-}
--- Tests/OverAssistantTests/OverAssistantTests.swift
+++ Tests/OverAssistantTests/OverAssistantTests.swift
@@ -3,14 +3,7 @@ import XCTest
 
 final class OverAssistantTests: XCTestCase {
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(OverAssistant().text, "Hello, World!")
-    }
-    
-    func testSelector() {
+    func testSelectorWithProfile() {
         ProfileProvider().loadProfile(nickname: "Xogsnim", platform: .psn)
         sleep(10)
         
@@ -44,7 +37,34 @@ final class OverAssistantTests: XCTestCase {
         print(HeroList.getHeroes(role: .support).map {$0.name} )
     }
     
+    func testSelectorWOProfile() {
+        var helper = PickHelper()
+        helper.conditions = GameConditions(map: nil, offenseSide: .symmetrical)
+        helper.typeOfQueue = .roleSpecific(.tank)
+        
+        helper.enemies = [HeroList[.tracer],
+                          HeroList[.mercy],
+                          HeroList[.winston],
+                          HeroList[.junkrat]]
+        
+        helper.allies = [HeroList[.soldier76],
+                         HeroList[.roadhog],
+                         HeroList[.dva]]
+        
+        let pickOptions = helper.getScoredHeroes()
+        let sortedByScore = pickOptions.sorted { firstPick, secondPick in
+            firstPick.value.value > secondPick.value.value
+        }
+        let topFive = sortedByScore.prefix(5)
+        for (hero, score) in topFive {
+            print(hero.name, score.value, score.sequence[.dps] ?? 0)
+        }
+        
+        print(HeroList.getHeroes(role: .support).map {$0.name} )
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testSelectorWithProfile", testSelectorWithProfile),
+        ("testSelectorWOProfile", testSelectorWOProfile),
     ]
 }
